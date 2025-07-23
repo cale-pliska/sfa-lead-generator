7bf3699d9de244e6900c604c1966a548d629d13f
Cale Pliska <59024810+cale-pliska@users.noreply.github.com>
2025-07-22 19:41:09 -0500

    Remove Step 4 functionality

--- steps/__init__.py
+++ steps/__init__.py
@@ -3,11 +3,9 @@
 from .step1 import step1_bp
 from .step2 import step2_bp
 from .step3 import step3_bp
-from .step4 import step4_bp
 
 __all__ = [
     "step1_bp",
     "step2_bp",
     "step3_bp",
-    "step4_bp",
 ]
